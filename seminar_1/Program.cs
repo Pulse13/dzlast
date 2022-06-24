1e555f1a4e4ed7b713e811b845493b425e68a21c
Pulse <shelvanek@mail.ru>
2022-06-24 15:07:34 +0300

    dz2

---
+++ seminar_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int f = Convert.ToInt32(Console.ReadLine());
+int K = 0;
+if(f>=100 && f<1000)
+{
+    K = f%10;
+    Console.WriteLine(K);
+}
+else
+{
+    Console.WriteLine("Ошибка.Введите трёхзначное число");
+}
