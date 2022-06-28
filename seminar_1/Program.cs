b37e63e958843e139bae0f4f50759c3d8fda30f4
Pulse <shelvanek@mail.ru>
2022-06-28 20:55:39 +0300

    dz1

--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите два числа: ");
+int f = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
+if (f>a)
+{
+    Console.WriteLine("max = " + f);
+}
+else
+{
+    Console.WriteLine("max = " + a);
+}
